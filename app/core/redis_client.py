454bd996c415bf819220f7516099db4f0af27c05
jinji <101579707+jinji12@users.noreply.github.com>
2025-08-21 18:53:20 -0600

    Updated files to reflect Docker changes

--- app/core/redis_client.py
+++ app/core/redis_client.py
@@ -1,7 +1,7 @@
 import redis
-from app.core.settings import settings
+from app.core.settings import REDIS_URL
 
-_redis_client = redis.Redis.from_url(settings.redis_url)
+_redis_client = redis.Redis.from_url(REDIS_URL)
 
 def get_redis_client():
     return _redis_client
